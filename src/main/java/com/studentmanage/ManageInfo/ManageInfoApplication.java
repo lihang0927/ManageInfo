2d18b93f1af4272513809864f4b8907b1c94dec4
yangwandi <1574112213@qq.com>
2019-05-28 11:07:04 +0800

    first commit

---
+++ src/main/java/com/studentmanage/ManageInfo/ManageInfoApplication.java
@@ -0,0 +1,13 @@
+package com.studentmanage.ManageInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageInfoApplication.class, args);
+	}
+
+}
