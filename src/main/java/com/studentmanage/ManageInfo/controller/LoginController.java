954f374943eebdf696b0dde9fec640a5678b530c
zsy <841932488@qq.com>
2019-05-30 17:16:31 +0800

    前端登陆页面第三个提交

--- src/main/java/com/studentmanage/ManageInfo/controller/LoginController.java
+++ src/main/java/com/studentmanage/ManageInfo/controller/LoginController.java
@@ -3,23 +3,42 @@ package com.studentmanage.ManageInfo.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.studentmanage.ManageInfo.model.UserLogin;
+import com.studentmanage.ManageInfo.repo.UserRepository;
+
+
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/login")//接口注解
 public class LoginController {
-	@GetMapping("/login")
-	public Map<String,Object> getAllColleges(){
+	
+	@Autowired
+	private UserRepository usrepo;//与dao（repository）层进行交互
+	
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	public Map<String,Object> getAllColleges(@RequestParam(required = false,value ="name") String name,@RequestParam(required = false,value ="password") String password){
 		Map<String,Object> map = new HashMap<String,Object>();
-		try {
+		Iterable<UserLogin> userlogindata= usrepo.findByNameAndPassword(name,password);
+		System.out.println(userlogindata);
+		if(userlogindata != null) {
 			map.put("result", true);
-		    map.put("data", "123");
-		    map.put("msg","成功登陆");
+		    map.put("statue", 200);
+		    map.put("msg","登陆成功");
 		    return map;
-		}catch(Exception e) {
+		}else{
 		     map.put("result", false);
+			 map.put("statue", 404);
 		     map.put("msg","登陆失败");
 		     return map;
 		}
